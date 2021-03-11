c7947d6422d5c01441a194a9df82ad163639da48
Mahmudulk675 <mahmudulk675@gmail.com>
2021-03-11 23:10:54 +0600

    14th

--- src/Components/DetailInfo/DetailInfo.js
+++ src/Components/DetailInfo/DetailInfo.js
@@ -53,9 +53,9 @@ const DetailInfo = (props) => {
             </div>
             <div className="social container">
                 <ul>
-                    <a href={`https://${strTwitter}`} target="_blank"><FontAwesomeIcon icon={faTwitterSquare} /></a>
-                    <a href={`https://${strFacebook}`} target="_blank"><FontAwesomeIcon icon={faFacebookSquare} /></a>
-                    <a href={`https://${strYoutube}`} target="_blank"><FontAwesomeIcon icon={faYoutubeSquare} /></a>
+                    <a href={`https://${strTwitter}`} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+                    <a href={`https://${strFacebook}`} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFacebookSquare} /></a>
+                    <a href={`https://${strYoutube}`} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faYoutubeSquare} /></a>
                 </ul>
             </div>
         </div>
